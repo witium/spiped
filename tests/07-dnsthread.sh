af55140d65628e004071774de63871e4d12ef94e
Graham Percival <gperciva@tarsnap.com>
2017-11-03 11:42:54 -0700

    REBASE dnsthread more test cases

--- tests/07-dnsthread.sh
+++ tests/07-dnsthread.sh
@@ -5,16 +5,21 @@
 
 ### Constants
 addr_output="${s_basename}-addrs.txt"
-
+addrs="	localhost:80
+	1.2.3.4:80
+	1:2:3:4:0:a:b:c:80
+	1:2:3:4::a:b:c:80
+	::1:80
+	/tmp/sock"
 
 ### Actual command
 scenario_cmd() {
-	# Check address
-	setup_check_variables
-	(
-		${dnsthread_resolve} localhost:80 > ${addr_output}
+	for addr in ${addrs}
+	do
+		setup_check_variables
+		${dnsthread_resolve} ${addr} >> ${addr_output}
 		echo $? > ${c_exitfile}
-	)
+	done
 
 	# Wait for server(s) to quit.
 	servers_stop
